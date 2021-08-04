2446fa3dd551c1459711539fb07f20a6c03e5d38
Roland Meyer <roland.meyer@beekeeper.io>
2021-08-04 19:03:38 +0200

    Convert mqtt actions to lowercase

--- buttonhub.py
+++ buttonhub.py
@@ -123,7 +123,7 @@ def handle_mqtt(_client, userdata, message):
 
     action_key = topics_config.get('action_key', 'action')
     if parsed_payload:
-        action = parsed_payload.get(action_key, 'default')
+        action = parsed_payload.get(action_key, 'default').lower()
 
     action_config = topics_config.get(action)
     if not action_config:
